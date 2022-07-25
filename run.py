b4283fe459c15fc4d3a15357856549db0e6e4cdb
Kam <58607149+spyderkam@users.noreply.github.com>
2022-07-25 14:19:19 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+   This script runs "brownian_bead" and records the positions
+   to one file and Rg & Ree to another file.
+"""
+
+
 from brownian_bead import *
 
-nbeads = 25
+nbeads = 20
+k_ev = 200
 
 Δt = .0001
 t = []
@@ -9,19 +19,20 @@ for i in range(N):
     t.append(tt)
     tt += Δt
 
-print(f"now running simulation for {nbeads} beads, {N} time-steps, each of size {Δt}...")
+print(f"[a] Simulating ideal chain: {nbeads} beads, N = {N}, Δt = {Δt}, k_ev = {k_ev}")
 
-sim = Simulation(nbeads=nbeads)
-sim.advance(Δt, κ_ev=0.0)
+sim = Simulation(nbeads)
+sim.advance(Δt, κ_ev=k_ev)
 
 
-###########################
-print("...now saving positions...")
+########################### 'write' only, no sense to ave pos, disable for multiple...
+print("[b] Saving coordinates")
 
 array0 = np.array(all_sim_pos)
-with open(f'path/to/nbeads{NoB}.npy', 'wb') as f:
+with open(f'pos_data/nbeads{nbeads}-k_ev{k_ev}.npy', 'wb') as f:
     np.save(f, array0)
-array1 = np.load(f'path/to/nbeads{NoB}.npy')
+array1 = np.load(f'pos_data/nbeads{nbeads}-k_ev{k_ev}.npy')
+
 
 # make sure arrays are the same
 comparison = (array0 == array1)     # don't need parantheses
@@ -35,9 +46,9 @@ Ree = []
 for xy in end_to_end:
     Ree.append(np.sqrt(xy[0]**2 + xy[1]**2))
 
-print("...now writing Rg and Ree to file")
+print("[c] Writing Rg & Ree")
 
-file = open(f"path/to/ave_rg_ree_n{NoB}.dat", "w")
+file = open(f"simulated_data/ave_rg_ree_n{nbeads}-k_ev{k_ev}.dat", "w")  # "a" vs "w"
 for i, s in enumerate(Rg):
 
     a = str( Rg[i] )
