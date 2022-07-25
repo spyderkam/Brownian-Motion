46905a914f03ad548815c1250b03d67a83c79c22
Kam <58607149+spyderkam@users.noreply.github.com>
2022-07-25 14:51:50 -0700

    Update hist.py

--- hist.py
+++ hist.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 nbeads = 20
 k_ev = 200
 
-arr = np.load(f'pos_data/nbeads{nbeads}-k_ev{k_ev}_a.npy')  # the original all_sim_pos for 50 
+arr = np.load(f'pos_data/nbeads{nbeads}-k_ev{k_ev}_a.npy')  
 N = len(arr[0])
 
-select_cos_arr = []; #np.zeros(nbeads-2)
+select_cos_arr = []; 
 
 ı_lst = []
 ı = 100; 
@@ -20,7 +18,6 @@ while ı < N:
         temp_x.append(bead[ı, 0])
         temp_y.append(bead[ı, 1])
     
-    
     vec_arr = np.array([])
     for i in range(1, nbeads):
         vec_arr = np.append(vec_arr, (temp_x[i]-temp_x[i-1], temp_y[i]-temp_y[i-1]))
@@ -28,21 +25,17 @@ while ı < N:
 
     cos_arr = np.array([])
     for i in range(1, nbeads-1):
-        γ = np.dot(vec_arr[i],vec_arr[i-1])/(np.linalg.norm(vec_arr[i])*np.linalg.norm(vec_arr[i-1]))
-        cos_arr = np.append(cos_arr, γ)#np.arccos(γ)*(180/np.pi)) # otherwise just γ
+        cos_γ = np.dot(vec_arr[i],vec_arr[i-1])/(np.linalg.norm(vec_arr[i])*np.linalg.norm(vec_arr[i-1]))
+        cos_arr = np.append(cos_arr, np.arccos(cos_γ)*(180/np.pi)) 
 
-     
-
-    
     select_cos_arr = np.append(select_cos_arr, cos_arr, axis=0)
     ı_lst.append(ı)
     ı += 100
 del ı; 
 
 
-
-plt.hist(select_cos_arr)#, bins=50)#, histtype='step')
-plt.xlabel(r"$\cos{\gamma}$", fontsize=20)
+plt.hist(select_cos_arr, bins=50)
+plt.xlabel(r"${\gamma}$", fontsize=20) # \cos{\gamma}
 plt.ylabel("frequency", fontsize=20)
 plt.tight_layout()
 plt.savefig(f"pos_data/pics/k_ev{k_ev}nbeads{nbeads}_histogram_c.png", bbox_inches='tight')
